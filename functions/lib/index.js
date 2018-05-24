b59ce6257cc391a3a4dd174a4cc631424636709e
Ozimek <albert.ozimek@gmail.com>
2018-05-24 12:30:01 +0200

    added example of clud function

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,9 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const functions = require("firebase-functions");
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+});
+exports.helloWorld2 = functions.https.onRequest((request, response) => {
+    response.send('Hello World 2');
+});
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,14 @@
 import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+// https://us-central1-leansoft-266b9.cloudfunctions.net/helloWorld
+export const helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+});
+
+// https://us-central1-leansoft-266b9.cloudfunctions.net/helloWorld2
+exports.helloWorld2 = functions.https.onRequest((request, response) => {
+  response.send('Hello World 2');
+});
