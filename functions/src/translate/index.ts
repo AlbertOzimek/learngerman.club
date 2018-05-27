2f2acc6cb075af2747011925eaec9ce1a234df84
Ozimek <albert.ozimek@gmail.com>
2018-05-27 13:14:09 +0200

    translateText cloud function

--- functions/src/translate/index.ts
+++ functions/src/translate/index.ts
@@ -1,7 +1,13 @@
 import * as functions from 'firebase-functions';
 
+
 // https://us-central1-leansoft-266b9.cloudfunctions.net/translateText
 export const translateText = functions.https.onRequest((request, response) => {
-  response.send("Hello from TranslateText cloud function!");
+  const apiKey = 'AIzaSyDATNKWDLv3yMoYaDUTLh_qLeC_AOC0k5I';
+  const lang = 'de';
+  const text = 'dog';
+  // https://www.googleapis.com/language/translate/v2?key=AIzaSyDATNKWDLv3yMoYaDUTLh_qLeC_AOC0k5I&source=en&target=de&q=dog
+  const translateUrl = `https://www.googleapis.com/language/translate/v2?key=${apiKey}&source=en&target=${lang}&q=${text}`;
+  response.send("Result of the translation will go here");
 });
 
