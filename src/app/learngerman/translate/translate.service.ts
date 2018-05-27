32be468f15ea50445e17df601ad00e9c3caf8c02
Ozimek <albert.ozimek@gmail.com>
2018-05-27 11:10:59 +0200

    cleaning translations service

--- src/app/learngerman/translate/translate.service.ts
+++ src/app/learngerman/translate/translate.service.ts
@@ -1,7 +1,5 @@
 import {Injectable} from '@angular/core';
 import {AngularFirestore} from 'angularfire2/firestore';
-import {AngularFirestoreCollection} from 'angularfire2/firestore/collection/collection';
-
 
 @Injectable()
 export class TranslateService {
@@ -9,13 +7,9 @@ export class TranslateService {
   constructor(private afs: AngularFirestore) { }
 
 
-  createTranslation(text: string): AngularFirestoreCollection<any> {
-    // create new translation, then return it as an object observable
-    const data = { 'english': text }
-
-    const key = this.afs.collection('/translations').add(data);
-
-    return this.afs.collection(`translations/${key}`);
+  createTranslation(text: string) {
+    const data = { 'english': text };
+    // this.afs.collection('/translations').add(data);
   }
 }
 
