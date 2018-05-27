d05f2ae9cebd896318cb86d0c47963255f1ad16b
Ozimek <albert.ozimek@gmail.com>
2018-05-27 12:02:15 +0200

    cloud functions minor change

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,12 +3,12 @@ import * as functions from 'firebase-functions';
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
+// https://us-central1-leansoft-266b9.cloudfunctions.net/translateText
+export const translateText = functions.https.onRequest((request, response) => {
+  response.send("Hello from TranslateText cloud function!");
+});
+
 // https://us-central1-leansoft-266b9.cloudfunctions.net/helloWorld
 export const helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
-});
-
-// https://us-central1-leansoft-266b9.cloudfunctions.net/helloWorld2
-exports.helloWorld2 = functions.https.onRequest((request, response) => {
-  response.send('Hello World 2');
 });
