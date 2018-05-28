8df610f7d3a92491aba750f969064be857927a0e
Ozimek <albert.ozimek@gmail.com>
2018-05-28 13:09:16 +0200

    added cloud functions from mimming/zero-to-app-universal-translator

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from 'firebase-functions';
+export * from './translate';
 
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
@@ -10,12 +11,23 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   response.send('Hello from Firebase!');
 });
 
-export * from './translate';
 
 // https://us-central1-leansoft-266b9.cloudfunctions.net/insertIntoDB?text=dog
 exports.insertIntoDB = functions.https.onRequest((request, response) => {
+  console.log('BBBBBBBBBBB - called insertIntoDB');
   const text = request.query.text;
   admin.database().ref('translations').push({'en': text}).then(snapshot => {
     response.redirect(303, snapshot.ref);
   });
 });
+
+exports.convertToUppercase = functions.database.ref('/test/{pushId}/text').onWrite(event => {
+  console.log('BBBBBBBBBBB - called convertToUppercase');
+  const text = event.before.val();
+  const uppercaseText = text.toUpperCase();
+  return event.after.ref.parent.child('uppercaseText').set(uppercaseText);
+});
+
+
+
+
