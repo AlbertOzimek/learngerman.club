dc3515c8692691bbf09627a7bd16592310f31d4f
Ozimek <albert.ozimek@gmail.com>
2018-07-17 12:48:33 +0200

    adjective deklination mit unbestimtenartikel

--- src/app/learngerman/grammar/deklination/adjective/adjective-deklination/adjective-deklination.component.ts
+++ src/app/learngerman/grammar/deklination/adjective/adjective-deklination/adjective-deklination.component.ts
@@ -1,12 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
 
 
 const DEFINITE_ARTIKLE_DATA: any[] = [
   {case: 'Nominativ', maskulin: 'der rote Stein', feminin: 'das helle Light', neutral: 'die klare Luft', plural: 'die runden Formen'},
   {case: 'Akkusativ', maskulin: 'den roten Stein', feminin: 'das helle Light', neutral: 'die klare Luft', plural: 'die runden Formen'},
   {case: 'Dativ', maskulin: 'dem roten Stein', feminin: 'dem hellen Light', neutral: 'der klaren Luft', plural: 'den runden Formen'},
-  {case: 'Genitiv', maskulin: 'des roten Steines', feminin: 'des hellen Lichtes', neutral: 'der Klaren Luft', plural: 'der runden Formen'},
+  {case: 'Genitiv', maskulin: 'des roten Steines', feminin: 'des hellen Lichtes', neutral: 'der klaren Luft', plural: 'der runden Formen'},
+];
+
+const INDEFINITE_ARTIKLE_DATA: any[] = [
+  {case: 'Nominativ', maskulin: 'ein roter Stein', feminin: 'ein helles Light', neutral: 'eine klare Luft', plural: 'runde Formen'},
+  {case: 'Akkusativ', maskulin: 'einen roten Stein', feminin: 'ein helles Light', neutral: 'eine klare Luft', plural: 'runde Formen'},
+  {case: 'Dativ', maskulin: 'eineem roten Stein', feminin: 'eindem hellen Light', neutral: 'einer klaren Luft', plural: 'runden Formen'},
+  {
+    case: 'Genitiv',
+    maskulin: 'eines roten Steines',
+    feminin: 'eines hellen Lichtes',
+    neutral: 'einer klaren Luft',
+    plural: 'runden Formen'
+  },
 ];
 
 @Component({
@@ -17,7 +29,13 @@ const DEFINITE_ARTIKLE_DATA: any[] = [
 export class AdjectiveDeklinationComponent implements OnInit {
   displayedColumns: string[] = ['case', 'maskulin', 'feminin', 'neutral', 'plural'];
   dataSource = DEFINITE_ARTIKLE_DATA;
-  constructor() { }
+
+  constructor() {
+  }
+
+  loadUndefiniteArtikleData() {
+    this.dataSource = INDEFINITE_ARTIKLE_DATA;
+  }
 
   ngOnInit() {
   }
