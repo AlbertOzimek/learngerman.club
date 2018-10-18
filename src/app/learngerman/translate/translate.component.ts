028e9624e50276a72f04e3ca3b607229d6bcf661
Ozimek <albert.ozimek@gmail.com>
2018-10-18 17:31:56 +0200

    clean code but for only one language

--- src/app/learngerman/translate/translate.component.ts
+++ src/app/learngerman/translate/translate.component.ts
@@ -1,18 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {TranslateService} from './translate.service';
-import {debounceTime, map} from 'rxjs/operators';
-import {Subject} from 'rxjs';
-
-interface TranslationObject {
-  data: {
-    translations;
-  };
-}
-
-interface Translation {
-  languageKey,
-  translatedText;
-}
+import {debounceTime, switchMap} from 'rxjs/operators';
+import {Observable, Subject} from 'rxjs';
 
 @Component({
   selector: 'learngerman-translate',
@@ -20,41 +9,23 @@ interface Translation {
   styleUrls: ['./translate.component.scss']
 })
 export class TranslateComponent implements OnInit {
-  targetLanguages: Array<string> = ['en', 'pl'];
-  translationArray = new Array<Translation>();
   userInput$ = new Subject();
+  translate$: Observable<Object>;
 
 
   constructor(private translateService: TranslateService) {
   }
 
   ngOnInit() {
-    this.userInput$
-      .pipe(debounceTime(500))
-      .subscribe((userInput: string) => {
-        this.translate(userInput);
-      });
+    this.translate$ = this.userInput$
+      .pipe(
+        debounceTime(500),
+        switchMap((userInput: string) => this.translateService.translate(userInput, 'en'))
+      );
   }
 
 
   onKeyUp(inputValue) {
     this.userInput$.next(inputValue);
-  }
-
-
-  translate(inputValue) {
-    this.targetLanguages.forEach((targetLanguage) => {
-      this.getTranslation(targetLanguage, inputValue);
-    });
-  }
-
-  getTranslation(targetLanguage: string, text: string) {
-    this.translateService.translate(text, targetLanguage)
-      .pipe(
-        map((translationObject: TranslationObject) => translationObject.data.translations[0].translatedText)
-      )
-      .subscribe((translatedText: string) => {
-        this.translationArray.push({languageKey: targetLanguage, translatedText: translatedText});
-      });
   }
 }
--- src/app/learngerman/translate/translate.service.ts
+++ src/app/learngerman/translate/translate.service.ts
@@ -2,20 +2,26 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {firebaseConfig} from '../firebase-config';
+import {map} from 'rxjs/operators';
+
+interface TranslationObject {
+  data: {
+    translations;
+  };
+}
 
 @Injectable()
 export class TranslateService {
 
-  private readonly translateApiUrl: string;
+  private readonly translateApiUrl = `https://www.googleapis.com/language/translate/v2?key=${firebaseConfig.apiKey}`;
 
   constructor(private httpClient: HttpClient) {
-    this.translateApiUrl = `https://www.googleapis.com/language/translate/v2?key=${firebaseConfig.apiKey}`;
   }
 
   translate(text: string, targetLanguage: string): Observable<Object> {
-    const queryUrl = `&source=de&target=${targetLanguage}&q=${text}`;
-    const url = this.translateApiUrl + queryUrl;
-    return this.httpClient.get(url);
+    return this.httpClient.get(`${this.translateApiUrl}&source=de&target=${targetLanguage}&q=${text}`) .pipe(
+      map((translationObject: TranslationObject) => translationObject.data.translations[0].translatedText)
+    );
   }
 }
 
