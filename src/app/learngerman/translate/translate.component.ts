878bf3cdaff6684008619e89c751ad501f76f034
Ozimek <albert.ozimek@gmail.com>
2018-10-18 16:22:58 +0200

    translate debounceTime

--- src/app/learngerman/translate/translate.component.ts
+++ src/app/learngerman/translate/translate.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {TranslateService} from './translate.service';
-import {map} from 'rxjs/operators';
+import {debounceTime, map} from 'rxjs/operators';
+import {Subject} from 'rxjs';
 
 interface TranslationObject {
   data: {
@@ -19,28 +20,41 @@ interface Translation {
   styleUrls: ['./translate.component.scss']
 })
 export class TranslateComponent implements OnInit {
-
-  userText: string;
   targetLanguages: Array<string> = ['en', 'pl'];
   translationArray: Array<Translation>;
+  userInput$ = new Subject();
 
 
   constructor(private translateService: TranslateService) {
   }
 
   ngOnInit() {
+    this.userInput$
+      .pipe(debounceTime(500))
+      .subscribe((userInput: string) => {
+        this.translate(userInput);
+      });
   }
 
-  translate($event) {
+
+  onKeyUp(inputValue) {
+    this.userInput$.next(inputValue);
+  }
+
+
+  translate(inputValue) {
     for (const targetLanguage of this.targetLanguages) {
-      this.getTranslation(targetLanguage, $event.target.value);
+      this.getTranslation(targetLanguage, inputValue);
     }
   }
 
   getTranslation(targetLanguage: string, text: string) {
     this.translationArray = new Array();
     this.translateService.translate(text, targetLanguage)
-      .pipe(map((translationObject: TranslationObject) => translationObject.data.translations[0].translatedText))
+      .pipe(
+        map((translationObject: TranslationObject) => translationObject.data.translations[0].translatedText),
+        debounceTime(50000)
+      )
       .subscribe((translatedText: string) => {
         this.translationArray.push({languageKey: targetLanguage, translatedText: translatedText});
       });
